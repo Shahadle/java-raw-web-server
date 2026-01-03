33714a081e7e770703d6bac39fb98a326e4a2dec
Shahad <shahadalelew4@gmail.com>
2026-01-02 19:04:28 -0600

    used try-with-resources for server socket

--- WebServer.java
+++ WebServer.java
@@ -12,62 +12,63 @@ public class WebServer {
       public static void main(String[] args) throws IOException {
             // Port the server will listen on
             int port = 4221;
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("Server listening on port:" + port);
+            try (ServerSocket serverSocket = new ServerSocket(port)){
+                  System.out.println("Server listening on port:" + port);
 
-            while (true) {
-                 try (Socket client = serverSocket.accept()) {
-                        System.out.println("Client connected.");
+                  while (true) {
+                  try (Socket client = serverSocket.accept()) {
+                              System.out.println("Client connected.");
 
-                        InputStream in = client.getInputStream();
-                        OutputStream out = client.getOutputStream();
+                              InputStream in = client.getInputStream();
+                              OutputStream out = client.getOutputStream();
 
-                        BufferedReader reader = new BufferedReader(
-                              new InputStreamReader(in, StandardCharsets.UTF_8)
-                        );
+                              BufferedReader reader = new BufferedReader(
+                                    new InputStreamReader(in, StandardCharsets.UTF_8)
+                              );
 
-                        // Read the HTTP request line
-                        String requestLine = reader.readLine();
-                        System.out.println("Request: " + requestLine);
+                              // Read the HTTP request line
+                              String requestLine = reader.readLine();
+                              System.out.println("Request: " + requestLine);
 
-                        String[] parts = requestLine.split(" ");
-                        // HTTP Method
-                        String method = parts[0];
-                        // Requested path
-                        String path = parts[1];
+                              String[] parts = requestLine.split(" ");
+                              // HTTP Method
+                              String method = parts[0];
+                              // Requested path
+                              String path = parts[1];
 
-                        String httpBody;
-                        int status = 200;
-                        String statusText = "OK";
+                              String httpBody;
+                              int status = 200;
+                              String statusText = "OK";
 
-                        if(!method.equals("GET")) {
-                              status = 405;
-                              statusText = "Method Not Allowed";
-                              httpBody = "<h1>405 Method Not Allowed</h1>";
-                        }else {
-                              Path filePath = Path.of("home.html");
+                              if(!method.equals("GET")) {
+                                    status = 405;
+                                    statusText = "Method Not Allowed";
+                                    httpBody = "<h1>405 Method Not Allowed</h1>";
+                              }else {
+                                    Path filePath = Path.of("home.html");
 
-                              if (Files.exists(filePath)) {
-                                    httpBody = Files.readString(filePath, StandardCharsets.UTF_8);
-                              } else {
-                                    status = 404;
-                                    statusText = "Not Found";
-                                    httpBody = "<h1>404 Not Found</h1>";
+                                    if (Files.exists(filePath)) {
+                                          httpBody = Files.readString(filePath, StandardCharsets.UTF_8);
+                                    } else {
+                                          status = 404;
+                                          statusText = "Not Found";
+                                          httpBody = "<h1>404 Not Found</h1>";
+                                    }
                               }
+
+                              byte[] bodyBytes = httpBody.getBytes(StandardCharsets.UTF_8);
+
+                              String headers =
+                                    "HTTP/1.1 " + status + " " + statusText + "\r\n" +
+                                    "Content-Type: text/html; charset=utf-8\r\n" +
+                                    "Content-Length: " + bodyBytes.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n";
+
+                              out.write(headers.getBytes(StandardCharsets.UTF_8));
+                              out.write(bodyBytes);
+                              out.flush();
                         }
-
-                        byte[] bodyBytes = httpBody.getBytes(StandardCharsets.UTF_8);
-
-                        String headers =
-                              "HTTP/1.1 " + status + " " + statusText + "\r\n" +
-                              "Content-Type: text/html; charset=utf-8\r\n" +
-                              "Content-Length: " + bodyBytes.length + "\r\n" +
-                              "Connection: close\r\n" +
-                              "\r\n";
-
-                        out.write(headers.getBytes(StandardCharsets.UTF_8));
-                        out.write(bodyBytes);
-                        out.flush();
                   }
             }
       }
